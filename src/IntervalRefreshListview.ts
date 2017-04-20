e8e6c0320198c8c3b5273b69b189441c33bac1c4
Andries Smit <mr.andries.smit@gmail.com>
2017-04-20 07:08:26 +0200

    Add allow update attribute to pause refreshes

--- src/IntervalRefreshListview.ts
+++ src/IntervalRefreshListview.ts
@@ -14,28 +14,88 @@ interface ListView extends mxui.widget._WidgetBase {
 class IntervalRefreshListview extends WidgetBase {
     targetName: string;
     refreshInterval: number;
+    allowRefreshAttribute: string;
 
     private targetWidget: ListView;
     private targetNode: HTMLElement | null;
     private timerHandle: number;
-
+    private isSetup: boolean;
+    private isRunning: boolean;
+    private allowRefresh: boolean;
+    private contextObject: mendix.lib.MxObject;
     postCreate() {
+        this.isRunning = false;
+        this.allowRefresh = true;
+        this.isSetup = false;
         this.targetNode = this.findTargetNode(this.targetName, this.domNode);
         if (this.targetNode) {
             this.targetWidget = registry.byNode(this.targetNode);
             if (this.isValidWidget(this.targetWidget)) {
                 domClass.add(this.targetNode, "widget-refresh-interval-listview");
-                this.setInterval();
+                this.isSetup = true;
+                // Startup if when allow refresh is not relevant, else wait for update;
+                if (!this.allowRefreshAttribute) {
+                    this.startRefreshing();
+                }
             }
         }
     }
 
+    update(contextObject: mendix.lib.MxObject, callback?: () => void ) {
+        this.contextObject = contextObject;
+        this.resetSubscription(contextObject);
+
+        if (contextObject && this.allowRefreshAttribute) {
+            this.updateRefresh();
+        } else if (this.allowRefreshAttribute) {
+            this.stopRefreshing();
+
+        }
+        if (callback) callback();
+    }
+
     uninitialize(): boolean {
+        this.stopRefreshing();
+
+        return true;
+    }
+
+    private updateRefresh() {
+        const allow = this.contextObject.get(this.allowRefreshAttribute) as boolean;
+        if (allow) {
+            this.startRefreshing();
+        } else {
+            this.stopRefreshing();
+        }
+    }
+
+    private startRefreshing() {
+        if (!this.isRunning) {
+            this.isRunning = true;
+            this.setInterval();
+        }
+    }
+
+    private stopRefreshing() {
+        this.isRunning = false;
         if (this.timerHandle) {
             window.clearInterval(this.timerHandle);
         }
+    }
 
-        return true;
+    private resetSubscription(mxObject: mendix.lib.MxObject) {
+        this.unsubscribeAll();
+        if (mxObject) {
+            this.subscribe({
+                callback: () => this.updateRefresh(),
+                guid: mxObject.getGuid()
+            });
+            this.subscribe({
+                attr: this.allowRefreshAttribute,
+                callback: () => this.updateRefresh(),
+                guid: mxObject.getGuid()
+            });
+        }
     }
 
     private findTargetNode(targetName: string, domNode: HTMLElement): HTMLElement | null {
@@ -92,7 +152,7 @@ class IntervalRefreshListview extends WidgetBase {
 // Declare widget prototype the Dojo way
 // Thanks to https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/dojo/README.md
 // tslint:disable : only-arrow-functions
-dojoDeclare("org.flockofbirds.widget.intervalrefreshlistview.IntervalRefreshListview",
+dojoDeclare("IntervalRefreshListview.widget.IntervalRefreshListview",
     [ WidgetBase ], function(Source: any) {
     const result: any = {};
     for (const property in Source.prototype) {
